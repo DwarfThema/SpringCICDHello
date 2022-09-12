5383bdeb03563f1d6e504f3e7740a94ae4980b21
Junho Park <dwarfthema@gmail.com>
2022-09-12 13:08:57 +0900

    init

---
+++ src/main/java/com/vivlepark/cicdhelloworld/CicdHelloworldApplication.java
@@ -0,0 +1,13 @@
+package com.vivlepark.cicdhelloworld;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CicdHelloworldApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(CicdHelloworldApplication.class, args);
+    }
+
+}
